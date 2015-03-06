17092dab8ef9b1aa63ffe074afa1bdaddc90216a
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-06 21:59:13 +0300

    Show and hide list by addition and removing.

--- source/scripts/wizard_tags.js
+++ source/scripts/wizard_tags.js
@@ -4,33 +4,32 @@ function WizardTags(element_query, options) {
 	var root = document.querySelector(element_query);
 	root.className = 'wizard-tags';
 
-	var list = document.createElement('ul');
-	list.className = 'wizard-tags-autocomplete-list';
-	if (typeof options.list_maximal_height !== "undefined") {
-		list.style.maxHeight = options.list_maximal_height + 'px';
-	}
-
+	var list = null;
 	var input = document.createElement('input');
 	input.addEventListener(
 		'focus',
 		function() {
-			list.style.display = 'block';
+			list = document.createElement('ul');
+			list.className = 'wizard-tags-autocomplete-list';
+			if (typeof options.list_maximal_height !== "undefined") {
+				list.style.maxHeight = options.list_maximal_height + 'px';
+			}
+
+			for (var i = 0; i < 10; i++) {
+				var item = document.createElement('li');
+				item.innerText = "ololo #" + i;
+
+				list.appendChild(item);
+			}
+
+			root.appendChild(list);
 		}
 	);
 	input.addEventListener(
 		'blur',
 		function() {
-			list.style.display = 'none';
+			root.removeChild(list);
 		}
 	);
 	root.appendChild(input);
-
-	for (var i = 0; i < 10; i++) {
-		var item = document.createElement('li');
-		item.innerText = "ololo #" + i;
-
-		list.appendChild(item);
-	}
-
-	root.appendChild(list);
 }
