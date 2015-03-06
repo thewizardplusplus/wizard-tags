c5b6e2c85bf56147ce1b1df80d22e51a5876e4a1
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-06 13:07:09 +0300

    Add options support.

--- source/scripts/wizard_tags.js
+++ source/scripts/wizard_tags.js
@@ -1,10 +1,14 @@
-function WizardTags(element_query) {
+function WizardTags(element_query, options) {
+	options = options || {};
+	options.list_maximal_height = options.list_maximal_height || 200;
+
 	var root = document.querySelector(element_query);
 	root.className = 'wizard-tags';
 	root.innerText = "Ololo!";
 
 	var list = document.createElement('ul');
 	list.className = 'wizard-tags-autocomplete-list';
+	list.style.maxHeight = options.list_maximal_height + 'px';
 
 	for (var i = 0; i < 10; i++) {
 		var item = document.createElement('li');
