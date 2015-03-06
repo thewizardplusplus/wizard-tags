89bcbd9409fcdbd5934560ee53fcea9d89b43e6c
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-06 13:00:02 +0300

    Using ul (list) tag as autocompletion list.

--- source/scripts/wizard_tags.js
+++ source/scripts/wizard_tags.js
@@ -1,17 +1,17 @@
 function WizardTags(element_query) {
 	var root = document.querySelector(element_query);
+	root.className = 'wizard-tags';
 	root.innerText = "Ololo!";
 
-	var select = document.createElement('select');
-	select.className = 'wizard-tags-select';
-	select.setAttribute('multiple', 'multiple');
+	var list = document.createElement('ul');
+	list.className = 'wizard-tags-autocomplete-list';
 
-	for (var i = 0; i < 5; i++) {
-		var option = document.createElement('option');
-		option.innerText = "ololo #" + i;
+	for (var i = 0; i < 10; i++) {
+		var item = document.createElement('li');
+		item.innerText = "ololo #" + i;
 
-		select.appendChild(option);
+		list.appendChild(item);
 	}
 
-	root.appendChild(select);
+	root.appendChild(list);
 }
