225398f76e1d6742903b9b6a902d4238002a7266
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-07 04:52:01 +0300

    Separate function for list control.

--- source/scripts/wizard_tags.js
+++ source/scripts/wizard_tags.js
@@ -20,32 +20,34 @@ function WizardTags(element_query, options) {
 	root.className = 'wizard-tags';
 
 	var list = null;
+	var CreateList = function(query) {
+		list = document.createElement('ul');
+		list.className = 'wizard-tags-autocomplete-list';
+		if (typeof options.list_maximal_height !== "undefined") {
+			list.style.maxHeight = options.list_maximal_height + 'px';
+		}
+
+		var tags = TagsGenerator(query);
+		for (var i = 0; i < tags.length; i++) {
+			var item = document.createElement('li');
+			item.innerText = tags[i];
+
+			list.appendChild(item);
+		}
+
+		root.appendChild(list);
+	};
+	var RemoveList = function() {
+		root.removeChild(list);
+	};
+
 	var input = document.createElement('input');
 	input.addEventListener(
 		'focus',
 		function() {
-			list = document.createElement('ul');
-			list.className = 'wizard-tags-autocomplete-list';
-			if (typeof options.list_maximal_height !== "undefined") {
-				list.style.maxHeight = options.list_maximal_height + 'px';
-			}
-
-			var tags = TagsGenerator(this.value);
-			for (var i = 0; i < tags.length; i++) {
-				var item = document.createElement('li');
-				item.innerText = tags[i];
-
-				list.appendChild(item);
-			}
-
-			root.appendChild(list);
+			CreateList(input.value);
 		}
 	);
-	input.addEventListener(
-		'blur',
-		function() {
-			root.removeChild(list);
-		}
-	);
+	input.addEventListener('blur', RemoveList);
 	root.appendChild(input);
 }
