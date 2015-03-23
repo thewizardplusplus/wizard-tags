3c511406cdfbc407eb69fa54aa2d9765e0e7e796
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-23 07:14:21 +0300

    Correct option processing.

--- source/scripts/wizard_tags.js
+++ source/scripts/wizard_tags.js
@@ -1,38 +1,45 @@
 var WizardTags = (function() {
-	var MakeDefaultTagsGenerator = function(tags) {
-		return function(query) {
-			return tags.filter(
-				function(tag) {
-					return tag.substr(0, query.length) == query;
-				}
-			);
+	var OptionsProcessor = (function() {
+		var MakeDefaultTagsGenerator = function(tags) {
+			return function(query) {
+				return tags.filter(
+					function(tag) {
+						return tag.substr(0, query.length) == query;
+					}
+				);
+			};
 		};
-	};
-	var GetTagsGenerator = function(options) {
-		var tags_generator = function() {
-			return [];
+		var GetTagsGenerator = function(options) {
+			var tags_generator = function() {
+				return [];
+			};
+			if (options.tags instanceof Function) {
+				tags_generator = options.tags;
+			} else if (options.tags instanceof Array) {
+				tags_generator = MakeDefaultTagsGenerator(options.tags);
+			}
+
+			return tags_generator;
 		};
-		if (options.tags instanceof Function) {
-			tags_generator = options.tags;
-		} else if (options.tags instanceof Array) {
-			tags_generator = MakeDefaultTagsGenerator(options.tags);
-		}
 
-		return tags_generator;
-	};
-	var ProcessOptions = function(options) {
-		var processed_options = options || {};
-		processed_options.tags = GetTagsGenerator(processed_options);
-		processed_options.separators = processed_options.separators || ' ';
-		processed_options.onChange =
-			processed_options.onChange
-			|| function() {};
+		return {
+			process: function(options) {
+				var processed_options = options || {};
+				processed_options.tags = GetTagsGenerator(processed_options);
+				processed_options.separators =
+					processed_options.separators
+					|| ' ';
+				processed_options.onChange =
+					processed_options.onChange
+					|| function() {};
 
-		return processed_options;
-	};
+				return processed_options;
+			}
+		};
+	})();
 
 	return function(root_element_query, options) {
-		options = ProcessOptions(options);
+		options = OptionsProcessor.process(options);
 		console.log(options);
 	};
 })();
